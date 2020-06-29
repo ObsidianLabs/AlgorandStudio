bc23c37056bddcc92ffa13e52c65a060e22c0ed3
Zehao Li <qft.gtr@gmail.com>
2020-06-29 15:05:49 +0800

    add BUILD to keypair

--- src.main/init.js
+++ src.main/init.js
@@ -1,16 +1,16 @@
 const { IpcChannel } = require('@obsidians/ipc')
+const KeypairManager = require('@obsidians/keypair')
 const { AutoUpdate } = require('@obsidians/algorand-welcome')
 const CompilerManager = require('@obsidians/algorand-compiler')
 const { InstanceManager } = require('@obsidians/algorand-instances')
-const KeypairManager = require('@obsidians/algorand-keypair')
 const ProjectChannel = require('@obsidians/algorand-project')
 
 let ipcChannel, autoUpdate, compilerManager, instanceManager, keypairManager, projectChannel
 module.exports = function () {
   ipcChannel = new IpcChannel()
+  keypairManager = new KeypairManager(process.env.BUILD)
   autoUpdate = new AutoUpdate('https://app.eosstudio.io/api/v1/check-update-algorand/')
   compilerManager = new CompilerManager()
   instanceManager = new InstanceManager()
-  keypairManager = new KeypairManager()
   projectChannel = new ProjectChannel()
 }
