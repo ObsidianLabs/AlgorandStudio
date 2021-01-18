110492c9f37f6a39325cfc7a5443183f842df568
Leon <7112003+leonlianght@users.noreply.github.com>
2021-01-18 10:50:39 +0800

    feat: desktop login (#13)
    * feat: desktop login
    
    * fix: missing authChannel

--- src.main/init.js
+++ src.main/init.js
@@ -4,8 +4,9 @@ const { AutoUpdate } = require('@obsidians/global')
 const CompilerManager = require('@obsidians/algorand-compiler')
 const { InstanceManager } = require('@obsidians/algorand-network')
 const ProjectChannel = require('@obsidians/algorand-project')
+const AuthChannel = require('@obsidians/auth')
 
-let ipcChannel, keypairManager, autoUpdate, compilerManager, instanceManager, projectChannel
+let ipcChannel, keypairManager, autoUpdate, compilerManager, instanceManager, projectChannel, authChannel
 module.exports = function () {
   ipcChannel = new IpcChannel()
   keypairManager = new KeypairManager(process.env.BUILD)
@@ -13,4 +14,5 @@ module.exports = function () {
   compilerManager = new CompilerManager()
   instanceManager = new InstanceManager()
   projectChannel = new ProjectChannel()
+  authChannel = new AuthChannel()
 }
